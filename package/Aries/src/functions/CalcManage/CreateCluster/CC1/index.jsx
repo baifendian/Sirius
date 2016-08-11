1da9f8a147cfe7cb4f2d8724d1a2f137defe0dc0
hyndanie <hyndaniel715@gmail.com>
2016-08-11 20:43:01 +0800

    微调样式

--- package/Aries/src/functions/CalcManage/CreateCluster/CC1/index.jsx
+++ package/Aries/src/functions/CalcManage/CreateCluster/CC1/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
-
+import './index.less'
 
 export default React.createClass({
   render(){
 
     return (
-      <div>
-         <ReactMarkdown source='# 入门
+      <div className='mdRootDiv'>
+         <ReactMarkdown className='mdChildDiv' source='# 入门
 云中心计算服务，是基于kubernetes的通用计算平台。可以轻松的调度Web服务，计算框架如storm, hadoop, tensorflow，中间件如redis, memcache, mongodb。任意可以作为docker镜像运行的程序或应用都可以呗调度在kubernetes之上，享受强大通用计算集群的能力。
 
 本文档将快速引导云中心计算服务的使用方法，并指导用户如何快速的在云端启动一个Hello World的Web应用
@@ -16,8 +16,8 @@ export default React.createClass({
 ### 下载kubectl客户端
 可以选择从以下链接下载对应的版本
 * OSX
-  * [官方v1.2.4](https://storage.googleapis.com/kubernetes-release/release/v1.2.4/bin/darwin/amd64/kubectl)
-  * [百分点镜像v1.2.4](http://127.0.0.1/更新这个链接)
+    * [官方v1.2.4](https://storage.googleapis.com/kubernetes-release/release/v1.2.4/bin/darwin/amd64/kubectl)
+
 * Linux
   * [官方v1.2.4](https://storage.googleapis.com/kubernetes-release/release/v1.2.4/bin/linux/amd64/kubectl)
   * [百分点镜像v1.2.4](http://127.0.0.1/更新这个链接)
