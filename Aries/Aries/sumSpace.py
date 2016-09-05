2e0f7a4d77b6e8871aae3a4cdedde1ac18daa68a
LuPan2015 <pan.lu@baifendian.com>
2016-09-05 20:48:09 +0800

    提交配额管理后端修改.

--- Aries/Aries/sumSpace.py
+++ Aries/Aries/sumSpace.py
@@ -8,23 +8,25 @@ sys.setdefaultencoding('utf-8')
 hdfs_logger = logging.getLogger("hdfs_log")
 
 def sumSpace():
+    hdfs_logger.info("##########sumSpace 统计开始.")
     spaces = Space.objects.all()
     for space in spaces:
         path = space.address
         exec_user = space.exec_user
         operator = "du"
         args = [path]
+        name = space.name
         capacity =json.load(space.capacity)
         exitCode,data = run_hadoop(user_name=exec_user,operator="du",args=args)
         if exitCode!=0:
             continue
             hdfs_logger.log("path:{0}.".format(path))
         else:
-            capacity["used"] = int(data)
-            hdfs_logger.info("path:{0},capacity:{1}".format(path,capacity))
+            capacity["used"] = int(data)/1024.0/1024/1024
+            hdfs_logger.info("spaceName:{0}, path:{1}, capacity:{2}".format(name,path,capacity))
             capacity = json.dumps(capacity)
             space.capacity = capacity
             space.save()
-        
+    hdfs_logger.info("###########sumSpace 统计结束.")
 if __name__ == "__main__"
     sumSpace()
