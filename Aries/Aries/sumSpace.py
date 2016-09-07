407d4b3465a928de682ba63dd6049dd40d8bebea
LuPan2015 <pan.lu@baifendian.com>
2016-09-07 19:28:50 +0800

    sumSpace提交

--- Aries/Aries/sumSpace.py
+++ Aries/Aries/sumSpace.py
@@ -3,30 +3,41 @@ from user_auth.models import *
 from django.conf import settings
 from hdfs.tools import *
 import sys
+import threading
+import time
+import json
 reload(sys)
 sys.setdefaultencoding('utf-8')
 hdfs_logger = logging.getLogger("hdfs_log")
 
-def sumSpace():
-    hdfs_logger.info("##########sumSpace 统计开始.")
-    spaces = Space.objects.all()
-    for space in spaces:
-        path = space.address
-        exec_user = space.exec_user
-        operator = "du"
-        args = [path]
-        name = space.name
-        capacity =json.load(space.capacity)
-        exitCode,data = run_hadoop(user_name=exec_user,operator="du",args=args)
-        if exitCode!=0:
-            continue
-            hdfs_logger.log("path:{0}.".format(path))
-        else:
-            capacity["used"] = int(data)/1024.0/1024/1024
-            hdfs_logger.info("spaceName:{0}, path:{1}, capacity:{2}".format(name,path,capacity))
-            capacity = json.dumps(capacity)
-            space.capacity = capacity
-            space.save()
-    hdfs_logger.info("###########sumSpace 统计结束.")
-if __name__ == "__main__"
-    sumSpace()
+def sumSpace(operator):
+    while True:
+        hdfs_logger.info("##########sumSpace 统计开始.")
+        spaces = Space.objects.all()
+        for space in spaces:
+            path = space.address
+            exec_user = space.exec_user
+            operator = "du"
+            args = [path]
+            name = space.name
+            capacity = eval(space.capacity)
+            try:
+                exitCode,data = run_hadoop(user_name=exec_user,operator=operator,args=args)
+            except Exception,e:
+                hdfs_logger.error(traceback.format_exc())
+            else:
+                if exitCode!=0:
+                    hdfs_logger.log("path:{0}.".format(path))
+                    continue
+                else:
+                    capacity["used"] = int(data)/1024.0/1024/1024
+                    hdfs_logger.info("spaceName:{0}, path:{1}, capacity:{2}".format(name,path,capacity))
+                    space.capacity = capacity
+                    space.save()
+        hdfs_logger.info("###########sumSpace 统计结束.")
+        time.sleep(600)
+
+def run():
+    t = threading.Thread(target=sumSpace,args=("du",))
+    t.start()
+
