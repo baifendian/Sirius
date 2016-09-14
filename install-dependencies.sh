8f6e16c3c7ebc6ba3e419f2bdf90d7934fafecbd
hyndanie <hyndaniel715@gmail.com>
2016-09-14 10:49:08 +0800

    modify

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,5 @@
 cd $PWD/package/Aries
 npm install
 mv $PWD/node_modules/bfd-ui/lib/Confirm $PWD/node_modules/bfd-ui/lib/confirm #解决bfd-ui0.7.7 confirm的bug
+#仅测试编译是否成功，不需要修改webpack.config.js文件
 npm run build
