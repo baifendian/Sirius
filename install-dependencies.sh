36b24d3efd78217f8e99cc4cebfe2223ba4872b6
hyndanie <hyndaniel715@gmail.com>
2016-09-14 18:57:59 +0800

    修改travis.yml文件，适配当前环境

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,5 +1,6 @@
 cd $PWD/package/Aries
 npm install
-mv $PWD/node_modules/bfd-ui/lib/Confirm $PWD/node_modules/bfd-ui/lib/confirm #解决bfd-ui0.7.7 confirm的bug
+#解决bfd-ui0.7.7 confirm的bug
+mv $PWD/node_modules/bfd-ui/lib/Confirm $PWD/node_modules/bfd-ui/lib/confirm 
 #仅测试编译是否成功，不需要修改webpack.config.js文件
 npm run build
