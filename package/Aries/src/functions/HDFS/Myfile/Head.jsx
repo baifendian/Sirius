dc7f9a51b31f24eb1b044b588ba660b8693ce710
LuPan2015 <874325293@qq.com>
2016-09-19 20:48:17 +0800

    上传操作添加进度条

--- package/Aries/src/functions/HDFS/Myfile/Head.jsx
+++ package/Aries/src/functions/HDFS/Myfile/Head.jsx
@@ -7,11 +7,31 @@ import message from 'bfd-ui/lib/message'
 
 const Head = React.createClass({
   source_path:"",
+  uploading:{
+    0:function(){
+        let width_px = 400 * (this.state.uploadNumber/100.0);
+        let percen = `${this.state.uploadNumber}%`;
+        let progress_bar = <div className="progress-bar">
+                              <div className="progress">
+                                <span className="green" style={{width:width_px}}>
+                                  <span>{percen}</span>
+                                </span>
+                              </div>
+                            </div>
+        return <div className="upload-modal">{progress_bar}</div>
+      }, //上传中的状态
+    1:()=>{return null}
+  },
   handleComplete(data){
     //上传完成处理
+    this.setState({isUploading:1,uploadNumber:0})
     message.success(data,2)
     let random = Math.floor(Math.random()*10000000000);
     this.props.updateRandom(random);
+  },
+  handleUploading(data){
+    console.log(`process: ${data}`);
+    this.setState({isUploading:0,uploadNumber:data})
   },
   changeAddess(path){
     console.log(path);
@@ -19,6 +39,12 @@ const Head = React.createClass({
   },
   mkdir(){
     this.props.addTableData();
+  },
+  getInitialState:function(){
+    return {
+      uploadNumber:0, //当前上传百分比
+      isUploading:1, //是否是上传中
+    }
   },
   render() {
     let uploadUrl = this.props.getUrlData({ type : "UPLOAD",
@@ -28,7 +54,9 @@ const Head = React.createClass({
     let props={
         action:uploadUrl,
         multiple: false,
-        onComplete:this.handleComplete
+        onComplete: this.handleComplete,
+        onUplading: this.handleUploading,
+        showFileList: false,
     }
 
     return (
@@ -41,6 +69,7 @@ const Head = React.createClass({
        <div className="table-div">
            <Upload className="table-div" {...props} />
        </div>
+       {this.uploading[this.state.isUploading].call(this)}
       </div>
     )
   }
