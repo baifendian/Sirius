5f2664baed9e45eec1f8bd85721923540c314c83
hyndanie <hyndaniel715@gmail.com>
2016-09-21 19:26:02 +0800

    travis适配bfd0.10、添加less模块，build命令添加参数bail

---
+++ package/Aries/src/functions/Openstack/Conf/Openstackconf.js
@@ -0,0 +1,49 @@
+import conf from 'public/Conf/Conf'
+const Openstackconf = {
+  //从location中获取cur_space
+  getCurSpace(_this) {
+    return conf.getCurSpace(_this);
+  },
+  //直接获取面包屑数据的接口
+  getNavigationData({
+    moduleName = "Instances",
+    pageName = "",
+    type = "",
+    spaceName = ""
+  }) {
+    return conf.getNavigationData({
+      moduleName: moduleName,
+      pageName: pageName,
+      type: type,
+      spaceName: spaceName
+    });
+  },
+  getNavigationDatav({
+    moduleName = "Volumes",
+    pageName = "",
+    type = "",
+    spaceName = ""
+  }) {
+    return conf.getNavigationData({
+      moduleName: moduleName,
+      pageName: pageName,
+      type: type,
+      spaceName: spaceName
+    });
+  },
+  getNavigationDatap({
+    moduleName = "Project",
+    pageName = "",
+    type = "",
+    spaceName = ""
+  }) {
+    return conf.getNavigationData({
+      moduleName: moduleName,
+      pageName: pageName,
+      type: type,
+      spaceName: spaceName
+    });
+  },
+}
+
+export default Openstackconf;
