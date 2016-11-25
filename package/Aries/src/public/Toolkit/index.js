fc58258accc2c7fd120a5dd487dfa186f8bdfa1e
郭攀 <pan.guo@baifendian.com>
2016-11-25 14:50:25 +0800

    基本完成了k8s概览页面的重构（封装了绘制echart图的模块），但是样式仍需调整

--- package/Aries/src/public/Toolkit/index.js
+++ package/Aries/src/public/Toolkit/index.js
@@ -5,22 +5,22 @@ var Toolkit = {
   /**
    * 对 oriValue 进行单位转换
    * oriValue(int)                原始数据值
-   * hex(int)                     进制（比如10、1024）
+   * base(int)                     进制（比如10、1024）
    * unitArr(Array)               单位字符串列表，如 ['B','KB','MB','GB','TB',]
    * significantFractionBit(int)  有效小数位位数
    */
-  unitConversion:function( oriValue,hex,unitArr,significantFractionBit ){
+  unitConversion:function( oriValue,base,unitArr,significantFractionBit ){
     let value = oriValue
     let unitIndex = 0
     while (1){
-      if ( value < hex ){     // 如果当前值小于hex，则说明无需再进行进制转换
+      if ( value < base ){     // 如果当前值小于base，则说明无需再进行进制转换
         break
       }
       if ( unitIndex >= unitArr.length-1 ){       // 如果发现提供的单位不够，则亦不会继续转换下去
         break
       }
       
-      value = value / hex
+      value = value / base
       unitIndex += 1
     }
     if (significantFractionBit){
