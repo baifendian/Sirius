3cb8845b3de29f7c681ee5f6718cb78de35669d6
郭攀 <pan.guo@baifendian.com>
2016-12-28 14:38:51 +0800

    资源用量-账单页面的调整

--- package/Aries/src/functions/CalcManage/ResourceUsageBilling/index.jsx
+++ package/Aries/src/functions/CalcManage/ResourceUsageBilling/index.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import FixedTable from 'bfd/FixedTable'
+import message from 'bfd/message'
 
+import MonthSelectControl from 'public/MonthSelectControl'
 import { AutoLayoutDiv , layoutInfoGenerator } from 'public/AutoLayout'
 import NavigationInPage from 'public/NavigationInPage'
 import Toolkit from 'public/Toolkit/index.js'
+import Button from 'bfd/Button'
 
 import CMDR from '../CalcManageDataRequester/requester.js'
 import CalcManageConf from '../UrlConf'
@@ -19,13 +22,7 @@ var mod = React.createClass({
   },
 
   getInitialState(){
-    let days = 90
-    let today_start = new Date(Toolkit.generateTimeStrByMilliSeconds(-1).slice(0,'YYYY-MM-DD'.length))
-    let start_date_str = Toolkit.generateTimeStrByMilliSeconds(today_start.getTime() - days*24*60*60*1000).slice(0,'YYYY-MM-DD'.length)
-
     return {
-      'days':days+1,      // +1，获取当天的用量
-      'startdate':start_date_str,
       'fixedTableDataList':[],
       'fixedTableHeight':0
     }
@@ -33,19 +30,9 @@ var mod = React.createClass({
   componentWillMount(){
     this.initUserData()
   },
-  componentDidMount(){
-    this.checkToRequestData()
-  },
-  componentDidUpdate(){
-    this.checkToRequestData()
-  },
 
-  checkToRequestData(){
-    let curNameSpace = CMDR.getCurNameSpace(this)
-    if ( this.curNameSpace !== curNameSpace ){
-      this.curNameSpace = curNameSpace
-      this.requestData()
-    }
+  componentDidMount(){
+    this.onSearchButtonClicked()
   },
 
   onHeightChanged(newHeight){
@@ -76,6 +63,11 @@ var mod = React.createClass({
     // 转换数据格式，方便 FixedTable 使用
     let toolTipInfo = this.userData['dataFormatterInfo']
     let fixedTableDataList = []
+
+    let totalBilling = 0.0
+    let totalCPU = 0.0
+    let totalMemory = 0.0
+
     for ( let i in executedData ){
       let curData = executedData[executedData.length-1-i]
 
@@ -85,8 +77,15 @@ var mod = React.createClass({
         'CPUUsage':this.renderCPUUsageValue(curData['data'][toolTipInfo['cpu']['key']]),
         'MemoryUsage':this.renderMemoryUsageValue(curData['data'][toolTipInfo['memory']['key']]),
       })
+      totalBilling += curData['data']['request']
+      totalCPU += curData['data'][toolTipInfo['cpu']['key']]
+      totalMemory += curData['data'][toolTipInfo['memory']['key']]
     }
     this.userData['fixedTableDataList'] = fixedTableDataList
+    this.userData['totalBilling'] = this.renderResourceUsageValue(totalBilling)
+    this.userData['totalCPU'] = this.renderCPUUsageValue(totalCPU)
+    this.userData['totalMemory'] = this.renderMemoryUsageValue(totalMemory)
+    return fixedTableDataList
   },
 
   initUserData(){
@@ -97,12 +96,16 @@ var mod = React.createClass({
 
     this.userData['divIDs'] = {
       rootDivID:Toolkit.generateGUID(),
-      navigationInPageID:Toolkit.generateGUID(),      
+      navigationInPageID:Toolkit.generateGUID(),
+      monthSelectControlID:Toolkit.generateGUID(),
+      billingTitleDivID:Toolkit.generateGUID(),
       FixedTableFatherDivID:Toolkit.generateGUID(),
     }
     this.userData['autoLayoutInfos'] = [
       layoutInfoGenerator( this.userData['divIDs']['rootDivID'],true ),
       layoutInfoGenerator( this.userData['divIDs']['navigationInPageID'],false,'Const' ),
+      layoutInfoGenerator( this.userData['divIDs']['monthSelectControlID'],false,'Const' ),
+      layoutInfoGenerator( this.userData['divIDs']['billingTitleDivID'],false,'Const' ),
       layoutInfoGenerator( this.userData['divIDs']['FixedTableFatherDivID'],false,'Var',( newHeight )=>{
         this.onHeightChanged( newHeight )
       } ),
@@ -132,21 +135,49 @@ var mod = React.createClass({
     }
   },
 
-
-  requestData(){
+  requestData( startDate,days ){
     this.setState({ 'fixedTableDataList':[] })
-    CMDR.getResourceUsageInfo(
-      CMDR.getCurNameSpace(this),
-      this.state.startdate,
-      this.state.days,
-      (executedData)=>{
-        this.resetResourceUsageData(executedData)
-        this.setState({ 'fixedTableDataList':this.userData['fixedTableDataList'] })
-      }
+    CMDR.getResourceUsageInfo( CMDR.getCurNameSpace(this),startDate,days,(executedData)=>{
+      this.resetResourceUsageData(executedData)
+      this.setState({ 'fixedTableDataList':this.userData['fixedTableDataList'] })
+    })
+  },
+
+  checkDateValid(dateStr){
+    let date = new Date(dateStr)
+    let curDate = new Date()
+    if ( date.getFullYear() > curDate.getFullYear() ){
+      return false
+    } else if ( date.getFullYear() == curDate.getFullYear() ){
+      return (date.getMonth() < curDate.getMonth())
+    } else {
+      return true
+    }
+  },
+
+  onSearchButtonClicked(){
+    let dateStr = this.refs.MonthSelectControlRef.getDate()
+    if ( !this.checkDateValid(dateStr) ){
+      message.danger( '指定月份的账单尚未生成' )
+      return
+    }
+
+    this.requestData( 
+      dateStr.slice(0,'YYYY-MM-DD'.length),
+      Toolkit.calcMonthDays(dateStr)
     )
   },
 
   render: function() {
+    let lastMonthDate = new Date(new Date().getTime() - Toolkit.calcMonthDays(new Date())*24*60*60*1000)
+    let defaultSearchDateStr = Toolkit.generateTimeStrByMilliSeconds(lastMonthDate.getTime()).slice( 0,'YYYY-MM'.length )
+
+    let dStr = this.refs.MonthSelectControlRef ? this.refs.MonthSelectControlRef.getValue() : defaultSearchDateStr
+    let billingTitle = Toolkit.strFormatter.formatString('{date} {nameSpace} 账单信息',{
+      'nameSpace':CalcManageConf.getCurSpace(this),
+      'date':dStr.split('-').join('年') + '月'
+    })
+
     return (
       <div id={this.userData['divIDs']['rootDivID']} className="ResourceUsageBillingRootDiv">
         <div id={this.userData['divIDs']['navigationInPageID']}>
@@ -161,9 +192,53 @@ var mod = React.createClass({
                               spaceName:CalcManageConf.getCurSpace(this)
                             })} />
         </div>
-        <div className="FixedTableFatherDiv" id={this.userData['divIDs']['FixedTableFatherDivID']}>
+        <div id={this.userData['divIDs']['monthSelectControlID']} className="MiddleBillingBlock">
+          <table className='SearchLayoutTable'>
+            <tbody>
+              <tr>
+                <td></td>
+                <td><MonthSelectControl defaultValue={defaultSearchDateStr} ref="MonthSelectControlRef" /></td>
+                <td></td>
+                <td><Button onClick={this.onSearchButtonClicked}>查询</Button></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div id={this.userData['divIDs']['billingTitleDivID']} className="MiddleBillingBlock">
+          <h2>{billingTitle}</h2>
+          <table className="BillingOverviewInfoTable">
+            <tbody>
+              <tr>
+                <td></td>
+                <td>空间</td>
+                <td></td>
+                <td>{CalcManageConf.getCurSpace(this)}</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>总账单</td>
+                <td></td>
+                <td>{this.userData['totalBilling']}</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>总CPU</td>
+                <td></td>
+                <td>{this.userData['totalCPU']}</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>总内存</td>
+                <td></td>
+                <td>{this.userData['totalMemory']}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="FixedTableFatherDiv MiddleBillingBlock" id={this.userData['divIDs']['FixedTableFatherDivID']}>
           <FixedTable className="FixedTable" ref="FixedTable" 
-                          height={ this.state.fixedTableHeight-90 }
+                          height={ this.state.fixedTableHeight-70 }
                           data={ this.state.fixedTableDataList }
                           showPage='false'
                           column={ this.userData['fixedTableColumn'] } />
